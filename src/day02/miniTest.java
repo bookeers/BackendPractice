8e7f43ba2a42c54684a036feb202510dd6dee048
sangjin.heo <adsoulkey@g.hongik.ac.kr>
2024-04-04 15:24:18 +0900

    20240404 실습 과제 제출.

---
+++ src/day02/miniTest.java
@@ -0,0 +1,51 @@
+package day02;
+
+public class miniTest {
+    public static void main(String[] args) {
+        Phone sonPhone = new Samsung("삐비빅");
+        Owner son = new Owner("야 내가 핸드폰 켜볼게", sonPhone);
+
+        son.turnOnPhone();
+
+        Phone beckhamPhone = new Apple("bbeep");
+        Owner beckham = new Owner("turn on", beckhamPhone);
+
+        beckham.turnOnPhone();
+    }
+}
+
+class Owner {
+    private String text;
+    private Phone phone;
+
+    Owner(String text, Phone phone) {
+        this.text = text;
+        this.phone = phone;
+    }
+
+    void turnOnPhone() {
+        System.out.println(text);
+        phone.turnOn();
+    }
+}
+
+class Phone {
+    String beepSound;
+
+
+    void turnOn() {
+        System.out.println(beepSound);
+    }
+}
+
+class Samsung extends Phone {
+    Samsung(String beepSound) {
+        this.beepSound = beepSound;
+    }
+}
+
+class Apple extends Phone {
+    Apple(String beepSound) {
+        this.beepSound = beepSound;
+    }
+}
